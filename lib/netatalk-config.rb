467a2897cfeb3410f06adfb88337386b0865c624
Jens Bissinger <mail@jens-bissinger.de>
2013-04-11 20:13:08 +0200

    implemented basic share afp.conf templating

--- lib/netatalk-config.rb
+++ lib/netatalk-config.rb
@@ -1,4 +1,26 @@
 require "netatalk-config/version"
 
 module NetatalkConfig
+  class << self
+    def afp
+      template.result binding
+    end
+
+    def shares
+      @shares ||= {}
+    end
+
+  private
+
+    def template
+      ERB.new File.read(__FILE__).split("__END__\n").last
+    end
+  end
 end
+
+__END__
+<% shares.each do |name, share| %>
+[<%= name %>]
+  path = <%= share['path'] %>
+  <% if share['time_machine'] == true %>time machine = yes<% end %>
+<% end %>
--- spec/lib/netatalk-config_spec.rb
+++ spec/lib/netatalk-config_spec.rb
@@ -1,4 +1,42 @@
 require "spec_helper"
+require "netatalk-config"
 
 describe NetatalkConfig do
+  context 'no shares' do
+    its('afp') { should match(/\A\s*\z/) }
+  end
+
+  shared_context 'simple share' do
+    before do
+      subject.shares['simple'] = {
+        'path' => '/simple/path'
+      }
+    end
+  end
+
+  shared_context 'advanced share' do
+    before do
+      subject.shares['advanced'] = {
+        'path' => '/advanced/path',
+        'time_machine' => true
+      }
+    end
+  end
+
+  context 'simple share' do
+    include_context 'simple share'
+    its('afp') { should include("[simple]\n  path = /simple/path") }
+  end
+
+  context 'advanced shares' do
+    include_context 'advanced share'
+    its('afp') { should include("[advanced]\n  path = /advanced/path\n  time machine = yes") }
+  end
+
+  context 'simple + advanced shares' do
+    include_context 'simple share'
+    include_context 'advanced share'
+    its('afp') { should include("[simple]\n  path = /simple/path") }
+    its('afp') { should include("[advanced]\n  path = /advanced/path\n  time machine = yes") }
+  end   
 end
